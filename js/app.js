e8facf35c6269032735892c1d8d8715ce0af2c67
Vladyslav <vladfrozzt@gmail.com>
2020-12-10 09:56:26 +0200

    Second lesson

--- js/app.js
+++ js/app.js
@@ -9,15 +9,71 @@
     genres: [],
     privat: false,
  };
- const
-     a = prompt("Один из последних просмотреных фильмов?", ""),
-     b = +prompt("На сколько оцените его?", ""),
-     c = prompt("Второй из последних просмотреных фильмов?", ""),
-     d = +prompt("На сколько оцените его?", "");
+
+ //  А теперь с помощью цикла for
+
+ for (let i = 0; i < 2; i++){
+    const
+    a = prompt("Один из последних просмотреных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+       console.log('dates saved!');
+    }
+    else {
+       i--;
+    }
+    PersonalMovieDB.movies[a] = b;
     
- PersonalMovieDB.movies[a] = b;
- PersonalMovieDB.movies[c] = d;
+ }
+
+
+
+
 
  console.log(PersonalMovieDB);
+
+
+ function ViewedFilms(){
+   if (PersonalMovieDB["count"] < 10 && PersonalMovieDB["count"]){
+      alert("Просмотрено мало фильмов");
+   }
+
+   else if (PersonalMovieDB["count"] < 30 && PersonalMovieDB["count"] > 9) {
+      alert("Вы классический зритель");
+   }
+
+   else if (PersonalMovieDB["count"] == "" || PersonalMovieDB["count"] == null){
+      alert("Произошла ошибка");
+   }
+
+   else {
+      alert("Вы киноман");
+   }
+ }
+
+ ViewedFilms();
+
+
+ let str = "test";
+ let word = "ATTENTION";
+ let text = "Some programm text";
+ console.log(str.toUpperCase());
+ console.log(word.toLowerCase());
+ console.log(word.indexOf("O"));
+ console.log(text.slice(5, 13));
+ console.log(text.slice(14));
+ console.log(text.substring(14));
+ console.log(text.substr(5, 8)); // Первый параметр указывает нам позицию, с которой мы начинаем вырезать текст, второй = кол-во символов, сколько мы хотим вырезать
+
+let num = 13.2;
+console.log(Math.round(num));
+
+let num = "15.5px";
+console.log(parseInt(num)); // округляет + преобразовует в число 
+console.log(parseFloat(num)); 
+
+
+
+ 
 
 
